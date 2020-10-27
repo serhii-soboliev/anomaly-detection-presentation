a893dd76ed7b856e6f66afa6afcbc4aad5f1a061
Serhii Soboliev <serhii.soboliev@datart.com>
2020-10-28 00:55:26 +0200

    Load data to Normalized Centroid Data

--- scripts/load_normalized_centroid_data_to_bq.sh
+++ scripts/load_normalized_centroid_data_to_bq.sh
@@ -3,4 +3,5 @@ export DATASET_NAME=work_data_ds
 bq load \
   --source_format=NEWLINE_DELIMITED_JSON \
   ${PROJECT_ID}:${DATASET_NAME}.normalized_centroid_data \
-  gs://anomaly-detection-presentation-data/normalized_centroid_data.json schemas/normalized_centroid_data_schema.json
+  gs://anomaly-detection-presentation-data/normalized_centroid_data.json \
+  schemas/normalized_centroid_data_schema.json
